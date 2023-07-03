caacfa280a9b65dca777acfb2b9a12b2aee3fabd
kwuite1738 <kwuite1738@gmail.com>
2023-07-02 22:30:12 -0700

    Making things work.

--- code/main.js
+++ code/main.js
@@ -3,53 +3,26 @@ Genesis.Entities = {}
 Genesis.Entities.Player = new Synthia.Entity();
 Genesis.Entities.Player.addComponent(Synthia.Components.Position)
 Genesis.Entities.Player.addComponent(Synthia.Components.VectorSprite)
-Genesis.Entities.Camera = new Synthia.Entity();
-Genesis.Entities.Camera.addComponent(Synthia.Components.Camera)
-Genesis.Entities.Camera.addComponent(Synthia.Components.Position)
-Genesis.Entities.Camera.addComponent(Synthia.Components.Tweenable)
-Genesis.Entities.Camera.addComponent(Synthia.Components.FollowTarget)
-Genesis.Renderer = {
-    Grid: function (display, entities) {
-        const gridSize = 50;
-        for (let entity of entities.values()) {
-            if (entity.hasComponent(Synthia.ComponentTypes.CAMERA)) {
-                let w = display.Canvas.width;
-                let h = display.Canvas.height;
-                let x = entity.Position.x
-                let y = entity.Position.y
-
-                for (let column = 0; column < w; column++) {
-                    if ((column + x) % gridSize < 1) {
-                        display.strokeLine(column, 0, column, h, 'black');
-                    }
-                }
-                for (let row = 0; row < h; row++) {
-                    if ((row + y) % gridSize < 1) {
-                        display.strokeLine(0, row, w, row, 'black')
-                    }
-                }
-                break;
-            }
-        }
-
-    } 
-}
+Genesis.Entities.Follows = new Synthia.Entity();
+Genesis.Entities.Follows.addComponent(Synthia.Components.VectorSprite)
+Genesis.Entities.Follows.addComponent(Synthia.Components.Camera)
+Genesis.Entities.Follows.addComponent(Synthia.Components.Position)
+Genesis.Entities.Follows.addComponent(Synthia.Components.Tweenable)
+Genesis.Entities.Follows.addComponent(Synthia.Components.FollowTarget)
 
 Genesis.Scenes = {
     PlayArea: new Synthia.Scene(
         "Play Area",
         {
             enter: function (info) {
-                this.addEntity(Genesis.Entities.Camera)
+                this.addEntity(Genesis.Entities.Follows)
                 this.addEntity(Genesis.Entities.Player)
                 this.Systems = [
                     Synthia.Systems.FollowTarget,
-                    Synthia.Systems.Tweening
+                    Synthia.Systems.Tweening,
+                    Synthia.Systems.BasicRenderer
                 ]
-                this.Renderers = [
-                    Genesis.Renderer.Grid,
-                    Synthia.Renderer.Vector
-                ]
+                this.Systems[2].Camera = Genesis.Entities.Follows
             }
         }
     )
